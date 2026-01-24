65ccea2d4cbfd86b9744973f6269459c090c3da8
Atan0707 <harizhakim85@gmail.com>
2026-01-24 19:24:43 +0800

    add data fetching for opendosm data

--- be/src/main.rs
+++ be/src/main.rs
@@ -1,3 +1,17 @@
+use prost::Message;
 fn main() {
-    println!("Hello, world!");
+    // prasarana_data();
 }
+
+// Data OpenDOSM Prasarana - guna protobuf
+#[tokio::main]
+async fn prasarana_data() {
+    let endpoint = "https://api.data.gov.my/gtfs-realtime/vehicle-position/prasarana?category=rapid-bus-kl";
+    let response = reqwest::get(endpoint).await.unwrap();
+    let body = response.bytes().await.unwrap();
+    let feed = gtfs_realtime::FeedMessage::decode(body).unwrap();
+    println!("Feed: {:?}", feed);
+}
+
+// Prasarana Websocket data
+
